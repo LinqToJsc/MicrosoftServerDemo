43e41f4e5491ac20c38711a0e0022356bb059951
jsc2412697426 <2412697426@qq.com>
2017-11-27 14:36:25 +0800

    JSC

---
+++ MicrosoftServerDemos/FaceAPIDemo/FaceApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaceAPIDemo
+{
+    public static class FaceApiConfig
+    {
+        public  const  string subscriptionKey = "13hc77781f7e4b19b5fcdd72a8df7156";
+
+
+        public const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/";
+    }
+}
