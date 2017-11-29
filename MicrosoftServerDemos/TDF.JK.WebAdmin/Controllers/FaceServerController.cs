1ef7a3d31fedcfe6f7546e5b7220c0e34685ed38
jsc2412697426 <2412697426@qq.com>
2017-11-29 18:36:11 +0800

    JSC

---
+++ MicrosoftServerDemos/TDF.JK.WebAdmin/Controllers/FaceServerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using TDF.JK.WebAdmin.Models.FaceApi;
+
+namespace TDF.JK.WebAdmin.Controllers
+{
+    public class FaceServerController : Controller
+    {
+        // GET: FaceServer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImgFaceDetect()
+        {
+            var files = Request.Files;
+            var file = files["FileModel"];
+            var imgStream = file.InputStream;
+
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", FaceApiConfig.subscriptionKey);
+
+
+            HttpResponseMessage response;
+
+
+            return Json("");
+        }
+    }
+}
---
+++ MicrosoftServerDemos/TDF.JK.WebAdmin/Models/FaceApi/FaceApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TDF.JK.WebAdmin.Models.FaceApi
+{
+    public class FaceApiConfig
+    {
+        // API Docm  https://westeurope.dev.cognitive.microsoft.com/docs/services/563879b61984550e40cbbe8d/operations/563879b61984550f3039523a
+
+        public const string subscriptionKey = "284b00a9e7d4491f9f0246d0ec0f42a1";
+
+
+        public const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/";
+    }
+}
